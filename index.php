ee9de5606be232ae41ef2f79b7bbab90da2c4446
Jules <you@example.com>
2021-08-13 18:48:03 +0200

    Version 0.3.5: Edit-icon bug fix

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ include_once "database_scripts/01_database_connection.php";
 ?>
 
 <!------------------------------------------------HEADER---------------------------------------------------------->
+
 <!DOCTYPE html>
 <html>
 <head>     
+
   <title>The Art of Music</title> 
 
   <!-- css-->
@@ -28,9 +30,9 @@ include_once "database_scripts/01_database_connection.php";
   <script 
     type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js">
   </script> 
-  
-  
+
 </head>
+
 <!------------------------------------------------BODY---------------------------------------------------------->
 <body background="img/background1.jpg">
 <h1 class="heading1">The Art of Music</h1>
@@ -64,6 +66,7 @@ include_once "database_scripts/01_database_connection.php";
 </form>
 
 <!-----------------------------------------------MAIN TABLE---------------------------------------------------------->
+
 <form id="addEntryForm" action="database_scripts/02_insertData.php" method="POST">
 <table id="table1" class="display">
   <thead>
@@ -83,7 +86,7 @@ include_once "database_scripts/01_database_connection.php";
       <th></th>
     </tr>
   </thead>
-  <tbody id="editor">       <!--  //make table body editable -->
+  <tbody id="editor">  <!--  //make table body editable -->
 
     <?php
 
@@ -91,16 +94,13 @@ include_once "database_scripts/01_database_connection.php";
         {
           $valueToSearch = $_POST['chords'];
           $sql = "SELECT * FROM `songs_chords` WHERE I='$valueToSearch' OR II='$valueToSearch' OR III='$valueToSearch' OR IV='$valueToSearch' OR V='$valueToSearch' OR VI='$valueToSearch' OR VII='$valueToSearch' OR VIII='$valueToSearch'";
-          
         }
         else {
           $sql = "SELECT * FROM songs_chords;";
-          $valueToSearch = "Bad Motherfucker";
-        
+          $valueToSearch = "Shenanigans";
         }
 
         
-
 
         $result=mysqli_query($connection,$sql);      //calling sql command
         $resultCheck=mysqli_num_rows($result);    //checking whether the command returns a result
@@ -168,13 +168,12 @@ include_once "database_scripts/01_database_connection.php";
         }
     ?>
 
-    
   </tbody>
 
-  <!----------------------- Table Footer for adding database records------------------------- -->
+  <!----------------------- Table Footer for adding database records------------------------------------------------------ -->
+  
   <tfoot>
     <tr>
-    
         <td><button id="addData" type="submit" name="addData">Add</button></td> 
         <td><input type="text" id="fucker" name="name" placeholder="Name"></td>
         <td><input type="number" name="BPM" placeholder="BPM"></td>
@@ -188,17 +187,14 @@ include_once "database_scripts/01_database_connection.php";
         <td><input type="text" name="VII" placeholder="VII"></td>
         <td><input type="text" name="VIII" placeholder="VIII"></td>
         <td></td>
-        
-  
-
     </tr>
   </tfoot>
 </table>
 </form>
 
 <!-- ---------------------------Modal Window for editing and deleting database records ------------------------------------------------------->
+
 <div id="myModal" class="modal">
-  <!-- Modal content -->
   <div class="modal-content">
     <span class="close">&times;</span>
     <form id="editForm" action="database_scripts/03_editData.php" method="POST">
@@ -245,20 +241,20 @@ include_once "database_scripts/01_database_connection.php";
 </div>
 
 <!---Snackbar which displays confirmation messages after adding, updating or deleting database records----------------------------------------------------- -->
-<div id="snackbar"></div>
 
+<div id="snackbar"></div>
 
 <!------------------------------------------ JavaScript------------------------------------------------------->
      
 <script>
 
-//calling jquery ready function to initialise  jquery table///////////
-  $(document).ready(function(){  
+  $(document).ready(function(){   //calling jquery ready function to initialise  jquery table
     $('#table1').DataTable();  
   });  
 
-///////////////// display edit/delete Modal when clicked on edit icon/////////////////////////////// 
-    
+//////////////////////////display Modal Window when User clicks on edit icon//////////////////////////////////
+
+var table=$('#table1').DataTable(); 
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
 
@@ -285,14 +281,14 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// Closw modal window when the user clicks anywhere outside of the modal
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-////////////////SnackBar Function/////////////////////////////
+////////////////SnackBar Message Function/////////////////////////////
 
 function SnackbarMessage(sessionVariable) {
   if (sessionVariable=="add"){
@@ -316,7 +312,7 @@ function SnackbarMessage(sessionVariable) {
  
 }
 
-////////////////////////////addEntry//////////////////////////////////////////////////////////
+//////////////////////////// addEntry //////////////////////////////////////////////////////////
 $('#addData').on( 'click', function (event) {
   if (confirm('Add this Entry?')) {
     document.forms["addEntryForm"].submit();
@@ -328,7 +324,7 @@ $('#addData').on( 'click', function (event) {
 
 
 
-////////////////////////////editData//////////////////////////////////////////////////////////
+//////////////////////////// editData //////////////////////////////////////////////////////////
 $('#saveChanges').on( 'click', function (event) {
   if (confirm('Save your changes?')) {
     document.forms["editForm"].submit();
@@ -338,7 +334,7 @@ $('#saveChanges').on( 'click', function (event) {
 }
 } );
 
-
+//////////////////////////// delete Data //////////////////////////////////////////////////////////
 $('#deleteEntry').on( 'click', function (event) {
   if (confirm('Are you sure you want to delete this entry?')) {
     document.forms["editForm"].submit();
@@ -352,10 +348,8 @@ $('#deleteEntry').on( 'click', function (event) {
 
 
 
-
-
-
 <!---------------------------------------- PHP script which checks for session variables--------------------------------------------------- -->
+
 <?php        
 
 if (isset($_SESSION['add_success'])) {
@@ -374,7 +368,6 @@ if (isset($_SESSION['update_success'])) {
 }
 
 ?>
-
 
 <!-- ------------ ------------------------------------------------------------------------------------->
 </body>
